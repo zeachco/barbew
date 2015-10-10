715d2e7a54a0b1fb7833ddef3901e4769100cbd4
FrancisL <lav.francis@gmail.com>
2015-10-09 21:29:40 -0400

    initial commit

---
+++ src/js/app.jsx
@@ -0,0 +1,44 @@
+
+// import * as jQuery from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import 'bootstrap-webpack';
+import '../css/main.css';
+
+var Contact = React.createClass({
+  displayName: 'Sapin',
+  render: function () {
+    return (
+      <li className="list-group-item">
+          <span>{this.props.name} - {this.props.address}</span>
+      </li>
+    )
+  }
+})
+
+var ContactList = React.createClass({
+  displayName: 'Sapin',
+  render: function () {
+
+    var contactList = [
+      {name: 'Bob', address: '45 Chemin du canal'},
+      {name: 'Mary', address: '1300 René Lévesque'}
+    ];
+
+    var contacts = [];
+
+    contactList.forEach(function(c){
+      contacts.push(<Contact name={c.name} address={c.address} />);
+    });
+
+    return (
+      <div className='container'>
+          <h1>Contacts</h1>
+          <ul className="list-group">{contacts}</ul>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<ContactList/>, document.getElementById('main-content'));
---
+++ webpack.conf.js
@@ -0,0 +1,23 @@
+module.exports = {
+	entry: './src/js/app.jsx',
+	output: {
+		path: 'dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+			{test: /\.(js|jsx)?$/, exclude: /(node_modules|bower_components)/, loader: 'babel'},
+			{test: /\.(woff|woff2)$/,  loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+	      	{test: /\.ttf$/,    loader: "file-loader" },
+	      	{test: /\.eot$/,    loader: "file-loader" },
+	      	{test: /\.svg$/,    loader: "file-loader" },
+			{test: /\.css$/,    loader: "style-loader!css-loader" }],
+		resolve: {
+			extensions: ['', '.js', '.jsx'],
+		}
+	}
+	// externals: {
+	// 	'jquery': 'jQuery'
+	// }
+};
